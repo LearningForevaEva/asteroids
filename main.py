abc26d4b91afd0edd00b0203c0467f6042f4ef05
learningforevaeva <foreverlearning8376@proton.me>
2025-10-04 19:08:55 -0700

    Add display screen with black background

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 from constants import *
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+BLACK = (0, 0, 0)
+
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(SCREEN_WIDTH)
     print(SCREEN_HEIGHT)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
