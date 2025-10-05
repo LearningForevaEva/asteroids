0c3c268167f5f59732f07aa4c9143931baa78327
learningforevaeva <foreverlearning8376@proton.me>
2025-10-04 20:08:19 -0700

    Add 60fps logic to use less CPU

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print("Starting Asteroids!")
     print(SCREEN_WIDTH)
     print(SCREEN_HEIGHT)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
         screen.fill(BLACK)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
