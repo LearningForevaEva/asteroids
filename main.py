d4fff026bbb1c123c686146e20749f900455a95b
learningforevaeva <foreverlearning8376@proton.me>
2025-10-04 22:47:19 -0700

    Add player rotation left and right on the a and d keys respectively

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
