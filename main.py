45d4e9c4999836fe36323a9d4fa642bdde93b8a1
learningforevaeva <foreverlearning8376@proton.me>
2025-10-05 18:41:46 -0700

    Add asteroids and asteroids field so asteroids appear on screen

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 BLACK = (0, 0, 0)
 x = SCREEN_WIDTH / 2
 y = SCREEN_HEIGHT / 2
+
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+AsteroidField.containers = (updatable,)
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
 player = Player(x, y)
+asteroidfield = AsteroidField()
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(SCREEN_WIDTH)
     print(SCREEN_HEIGHT)
